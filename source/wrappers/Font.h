0156098a7fa5a724100206ae8a3cb35196ba846f
samf547 <sdfisher79@gmail.com>
2019-04-28 21:39:04 -0400

    Updated Font.h

--- source/wrappers/Font.h
+++ source/wrappers/Font.h
@@ -46,50 +46,6 @@ class Font {
 								SDL_Rect* clip = NULL, double angle = 0.0, 
 								SDL_Point* center = NULL, 
 								SDL_RendererFlip flip = SDL_FLIP_NONE) {
-									
-									
-			/* TESTING */
-			/* removed for now cause its dumb
-			std::string test = " ";
-			std::size_t temp = 0;
-			temp = text.rfind(test);
-			if (temp != std::string::npos && temp < text.length()) {
-				std::stringstream help;
-				help.str(text);
-				help << "------";
-				int* tempX = new int(0);
-				int* tempY = new int(0);
-				TTF_SizeText(this->fontRenderer, help.str().c_str(), tempX, tempY);
-				if (*tempX % maxWidth > maxWidth - maxWidth/4) {
-					help.str("");
-					for (int i = 0; i < text.length(); i++) {
-						help << text[i];
-						if (i == temp) {
-							help << "\n";
-						}
-					}
-					text = help.str();
-				}
-				delete tempX;
-				delete tempY;
-			}*/
-			/*
-			std::stringstream help;
-			help.str(text);
-			help << "--";
-			int* tempX = new int(0);
-			int* tempY = new int(0);
-			TTF_SizeText(this->fontRenderer, help.str().c_str(), tempX, tempY);
-			if (*tempX % maxWidth > maxWidth - maxWidth / 4) {
-				std::string test = " ";
-				std::size_t temp = 0;
-				temp = text.rfind(test);
-				if (temp != std::string::npos)
-					text.insert(temp, "\n");
-			}
-			delete tempX;
-			delete tempY;
-			*/
 			SDL_Surface *surface = TTF_RenderText_Blended_Wrapped(fontRenderer, text.c_str(), color, maxWidth);
 			if (!(surface == NULL)) {
 				SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
