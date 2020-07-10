933c828a5f7c6180cb8e7ae52bea9d578cefe834
Mani Kamali <kamalisarvestani.1@themetroschool.org>
2020-07-09 21:18:35 -0400

    Font check if exists

--- source/wrappers/Font.cpp
+++ source/wrappers/Font.cpp
@@ -1,11 +1,11 @@
 #include "Font.h"
 
 Font::Font(int size, std::string filename) {
-	this->filename = "/resources/" + filename;
+	this->filename = "./resources/" + filename;
 	fontSize = size;
 	fontRenderer = TTF_OpenFont(this->filename.c_str(), size);
-	if (fontRenderer == NULL) {
-		printf("Failed to load font\n");
+	if (!fontRenderer) {
+        printf("TTF_OpenFont: %s\n", TTF_GetError());
 	}
 }
 
@@ -32,7 +32,11 @@ void Font::drawTexture(SDL_Renderer* renderer, SDL_Texture* texture, SDL_Rect re
 }
 
 void Font::drawText(int x, int y, std::string text, SDL_Renderer* renderer, SDL_Color color, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip) {
-	SDL_Surface* surface = TTF_RenderText_Blended(fontRenderer, text.c_str(), color);
+	if (!fontRenderer) {
+        printf("TTF_OpenFont: %s\n", TTF_GetError());
+        return;
+	}
+	SDL_Surface *surface = TTF_RenderText_Blended(fontRenderer, text.c_str(), color);
 	if (!(surface == NULL)) {
 		SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 		if (!(texture == NULL)) {
@@ -45,6 +49,9 @@ void Font::drawText(int x, int y, std::string text, SDL_Renderer* renderer, SDL_
 }
 
 void Font::drawTextWrapped(int x, int y, std::string text, SDL_Renderer* renderer, SDL_Color color, int maxWidth, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip) {
+    if (this->fontRenderer == NULL) {
+        return;
+    }
 	SDL_Surface* surface = TTF_RenderText_Blended_Wrapped(fontRenderer, text.c_str(), color, maxWidth);
 	if (surface != NULL) {
 		SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
