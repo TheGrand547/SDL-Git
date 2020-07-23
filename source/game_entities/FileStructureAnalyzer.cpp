e2bd1b23235178987c5e0ac2a349465189014d40
samf547 <sdfisher79@gmail.com>
2020-07-22 23:38:43 -0400

    Updated Analyzer
    Fuck this gave me a headache

--- source/game_entities/FileStructureAnalyzer.cpp
+++ source/game_entities/FileStructureAnalyzer.cpp
@@ -1,29 +1,42 @@
 #include "FileStructureAnalyzer.h"
 #include "Sector.h"
 #include "SectorGroup.h"
+#include "BigWall.h"
 #include "../essential/log.h"
+#include<map>
 
 typedef std::string::size_type StringPos;
+typedef std::map<std::string, std::string> StringMap;
 
 Rect rectFromString(const std::string& string) {
 	try {
-		StringPos next;
+		StringPos next = 0;
 		std::string sub = string.substr(string.find("Rect") + 5);
-		double vals[4];
-		for (int i = 0; i < 4; i++) {
-			vals[i] = std::stod(sub, &next);
-			sub = sub.substr(next);
+		std::vector<double> vals;
+		while (next != std::string::npos) {
+			vals.push_back(std::stod(sub, &next));
+			sub = sub.substr(next + 1);
+			auto pp = sub.find_last_of(" \f\t\v");
+			if (pp == std::string::npos) {
+				vals.push_back(std::stod(sub));
+				break;
+			}
 		}
-		return Rect(vals[0], vals[1], vals[2], vals[3]);
+		if (vals.size() >= 8) return Rect(Line(Point(vals[0], vals[1]), Point(vals[2], vals[3])), Line(Point(vals[4], vals[5]), Point(vals[6], vals[7])));
+		if (vals.size() >= 4) return Rect(vals[0], vals[1], vals[2], vals[3]);
 	} catch (...) {
 		return Rect();	
 	}
+	return Rect();
 }
 
 void analyzeFile(const std::string& source, GameInstance& instance) {
 	std::ifstream file(source);
 	std::string line, sub;
 	int lineNumber = 0;
+	
+	StringMap things;
+	
 	while (std::getline(file, line)) {
 		lineNumber++;
 		StringPos begin = line.find_first_not_of(" \f\t\v");
@@ -43,8 +56,19 @@ void analyzeFile(const std::string& source, GameInstance& instance) {
 				else LOG("Improperly formatted 'Rect' on line %i of %s.", lineNumber, source.c_str());
 				continue;
 			}
+		} else if ((type = line.find("BasicWall", begin)) != std::string::npos) { // Big Wall
+			Rect rect(rectFromString(line));
+			if (rect.isReal()) instance.createThing<BigWall>(rect);
+			else LOG("Improperly formatted 'Rect' on line %i of %s.", lineNumber, source.c_str());
+			continue;
+		} else if ((type = line.find("Thing", begin)) != std::string::npos) { // Definition of a thing
+			std::cout << "We makin a thing" << std::endl;
 		}
 		LOG("Line %i of %s is improperly formatted for analysis.", lineNumber, source.c_str());	
 	}
 	file.close();
+}
+
+void analyzeStructure(const std::string& source, GameInstance& instance, StringMap& map) {
+	std::cout << source << "->" << &instance << std::endl;
 }
--- source/game_entities/FileStructureAnalyzer.h
+++ source/game_entities/FileStructureAnalyzer.h
@@ -6,9 +6,12 @@
 #include<fstream>
 #include<string>
 
+typedef std::map<std::string, std::string> StringMap;
+
 Rect rectFromString(const std::string& string);
 
 // Takes a file input and creates all the things in the level from it
 void analyzeFile(const std::string& source, GameInstance& instance);
+void analyzeStructure(const std::string& source, GameInstance& instance, StringMap& map);
 
 #endif
