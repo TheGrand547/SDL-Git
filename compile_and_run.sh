ec588acbb24c2b15ba4724ea4a3670f5ab3a8d2f
Jakobch <christoffersson.jakob@gmail.com>
2020-08-01 15:48:06 +0200

    Changes acording to THE GUY

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-mkdir build 2> /dev/null
-meson build && cd build && meson compile && ./source/Main
+
+
+if [ ! -f ./build/build.ninja ]; then
+	meson build
+fi
+
+ninja -C ./build/ && ./build/source/Main
+
 
