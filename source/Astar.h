77f8a03436738261c084a6a5841eb73bdcbce1dd
samf547 <sdfisher79@gmail.com>
2020-11-12 23:56:56 -0500

    Astar updated
    Working on full template integration, and we'll get there ~monday I think

--- source/Astar.h
+++ source/Astar.h
@@ -10,59 +10,69 @@ struct VALUE {
 };
 
 namespace AStar {
-	template<class T> std::vector<T> generatePath(const T& start, const T& end, double(*heuristic)(const T&, const T&)) {
+	template<typename T> struct Node {
+		T value;
+		bool contains(const Node<T>& t) {
+			return ((std::size_t)&t) % 3; 
+		}
+	};
+
+	template<typename T> std::vector<T> generatePath(const Node<T>& start, const Node<T>& end, double(*heuristic)(const T&, const T&), 
+													double(*edgeHeuristic)(const T&, const T&)) {
+		using TVector = std::vector<Node<T>>;
 		// A* implementation
-		this->clear();
-		std::vector<T> vector;
-		if (startingSector.get() == target.get()) return;
-		if (startingSector->contains(target.get())) {
-			this->stored = {startingSector, target};
-			return;
+		TVector result;
+		if (start == end) {
+			result = {start};
+		} else if (start->contains(end)) {
+			result = {start, end};
+		} else {
+			TVector unused = {start};
+			std::map<T*, T*> path;
+			path[start] = NULL;
+	
+			std::map<T*, VALUE> cost;
+			cost[start].value = 0;
+	
+			std::map<T*, VALUE> currentCost;
+			currentCost[&start].value = h(start.data, end.data);
+	
+			while (unused.size() > 0) {
+				typename TVector::iterator index = unused.begin();
+				for (std::size_t i = 0; i < unused.size(); i++) {
+					if (currentCost[unused.begin()[i]].value < currentCost[*index].value) {
+						index = unused.begin() + i;
+					}
+				}
+				if (*index == end) {
+					TVector temp;
+					typename std::map<T*, T*>::iterator it = path.find(*index);
+					while (it != path.end()) {
+						temp.push_back(it->first);
+						it = path.find(it->second);
+					}
+					typename TVector::reverse_iterator iter = temp.rbegin();
+					for (; iter != temp.rend(); iter++) {
+						result.push_back(iter[0]);
+					}
+					break;
+				}
+				Node<T>* current = *index;
+				unused.erase(index);
+				for (auto& weak: current->attached()) {
+					Node<T>* node = weak;
+					if (!node) continue;
+					double general = cost[current].value + edgeFunction(node, current);
+					if (general < cost[node].value) {
+						path[node] = current;
+						cost[node].value = general;
+						currentCost[node].value = general + getValue(node, end);
+						if (valueNotInVector(unused, node)) unused.push_back(node);
+					}
+				}
+			}
 		}
-		SectorVector unused = {startingSector};
-		std::map<SectorPtr, SectorPtr> path;
-		path[startingSector] = NULL;
-
-		std::map<SectorPtr, VALUE> cost;
-		cost[startingSector].value = 0;
-
-		std::map<SectorPtr, VALUE> currentCost;
-		currentCost[startingSector].value = getValue(startingSector, target);
-
-		while (unused.size() > 0) {
-			SectorVector::iterator index = unused.begin();
-			for (std::size_t i = 0; i < unused.size(); i++) {
-				if (currentCost[unused.begin()[i]].value < currentCost[*index].value) {
-					index = unused.begin() + i;
-				}
-			}
-			if (*index == target) {
-				SectorVector temp;
-				std::map<SectorPtr, SectorPtr>::iterator it = path.find(*index);
-				while (it != path.end()) {
-					temp.push_back(it->first);
-					it = path.find(it->second);
-				}
-				SectorVector::reverse_iterator iter = temp.rbegin();
-				for (; iter != temp.rend(); iter++) {
-					this->stored.push_back(iter[0]);
-				}
-				break;
-			}
-			SectorPtr current = *index;
-			unused.erase(index);
-			for (std::weak_ptr<SectorBase> weak: current->attached()) {
-				SectorPtr node = weak.lock();
-				if (!node) continue;
-				double general = cost[current].value + edgeFunction(node, current);
-				if (general < cost[node].value) {
-					path[node] = current;
-					cost[node].value = general;
-					currentCost[node].value = general + getValue(node, target);
-					if (valueNotInVector(unused, node)) unused.push_back(node);
-				}
-			}
-		}
+		return result;
 	}
 }
 
